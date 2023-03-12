7727cf6bc852df7df47739bf80c50b9afaa30d80
Ann18532 <122373286+Ann18532@users.noreply.github.com>
2023-03-12 22:41:59 +0530

    Update tele_bot.py

--- bot-codes/tele_bot.py
+++ bot-codes/tele_bot.py
@@ -1,24 +1,28 @@
-from telegram import Poll,ChatAction
+
 from telegram.ext import (
     Updater,
     CommandHandler,
     MessageHandler,
     Filters,
     CallbackQueryHandler,
+    CallbackContext,
     PollHandler,
+    PollAnswerHandler
 )
+from api import apiView
 
-from fetch import QuizQuestion
-import api
+
 import logging
 import logging.config
 import os
-import telegram
+from telegram import *
 import requests
 import json
 
 import time
 import datetime
+user_scores={}
+correct_id=0
 def main_handler(update, context):
     logging.info(f"update : {update}")
 
@@ -36,31 +40,71 @@ def help_command_handler(update, context):
 
 
 def poll_handler(update, context):
-    logging.info(f"question : {update.poll.question}")
-    logging.info(f"correct option : {update.poll.correct_option_id}")
-    logging.info(f"option #1 : {update.poll.options[0]}")
-    logging.info(f"option #2 : {update.poll.options[1]}")
-    logging.info(f"option #3 : {update.poll.options[2]}")
-
     user_answer = get_answer(update)
-    logging.info(f"correct option {is_answer_correct(update)}")
+    # logging.info(update)
+    # correct_id=update.poll.correct_option_id
+    # user_id = update.poll.user.id
+    # option_ids = update.poll.option_ids
+    # score = len(option_ids) # Each selected option is worth 1 point
+    # update_score(user_id, score)
+    
+    # logging.info(f"correct option {is_answer_correct(update)}")
+    # add_typing(update, context)
+    # add_text_message(update, context, f"Correct answer is {update.poll.options[update.poll.correct_option_id]['text']}")
 
-    add_typing(update, context)
-    add_text_message(update, context, f"Correct answer is {user_answer}")
 
 
 def start_command_handler(update, context):
-    update.message.reply_text("Please enter the number of questions you want to answer")
+    update.message.reply_text("Please enter the number of questions you want to answer(1-10) in the format /quiz<space>number of questions")
+    
+    # for i in range(0,4):
+    #     dict=apiView.apiCall()
+    #     add_typing(update, context)
+    #     add_quiz_question(update,context,dict)  
+    #     time.sleep(0.5)
+        
+    
+    
 
     
-    # b=int(input())
-    # for i in range (0,b):
-   
-    dict=QuizQuestion.questions()
-    add_typing(update, context)
-            
+def quiz_command_handler(update,context):
+    # try:
+        Number_of_questions=int(context.args[0])
+        for i in range (0,Number_of_questions):
+            dict=apiView.apiCall()
+            add_typing(update, context)
+            add_quiz_question(update,context,dict)
+            add_typing(update, context)
+            time.sleep(0.5)
+        display_scores(update,context)
+    # except:
+    #     add_text_message(update, context, f"Please provide the correct format")
 
-    add_quiz_question(update,context,dict)
+
+
+def poll_answer_handler(bot,update):
+    logging.info(update)
+    user_id = update.poll_answer.user.id
+    option = update.poll_answer.option_ids[0]
+    
+    # Calculate the user's score based on the selected option
+    score = 0
+    if option==correct_id:
+        score = 1
+    
+    # Add the user's score to the user_scores dictionary
+    if user_id not in user_scores:
+        user_scores[user_id] = 0
+    user_scores[user_id] += score
+
+# Define a function to display the scores
+def display_scores(update,context):
+    scores_text = ""
+    for user_id, score in user_scores.items():
+        
+        scores_text += f"Quiz scores\n:{user_id},{score}\n"
+    add_text_message(update,context,scores_text)
+
 
 
 
@@ -68,8 +112,8 @@ def add_quiz_question(update, context ,dict):
     
     message = context.bot.send_poll(
         chat_id=get_chat_id(update, context),
-        question=dict[0],
-        options=dict[4],
+        question=dict[2],
+        options=dict[1],
         type=Poll.QUIZ,
         correct_option_id=dict[3],
         open_period=8,
@@ -78,7 +122,7 @@ def add_quiz_question(update, context ,dict):
     )
 
     # Save some info about the poll the bot_data for later use in receive_quiz_answer
-    context.bot_data.update({message.poll.id: message.chat.id})
+    # context.bot_data.update({message.poll.id: message.chat.id})
 
 
 def add_typing(update, context):
@@ -87,11 +131,9 @@ def add_typing(update, context):
         action=ChatAction.TYPING,
         timeout=1,
     )
-    time.sleep(1)
+    time.sleep(0.7)
 
-def no_of_questions(user_input):
-    s=user_input
-    return s
+
 
 
 def add_text_message(update, context, message):
@@ -114,7 +156,7 @@ def get_answer(update):
     return ret
 
 
-def is_answer_correct(update):
+def is_answer_correct(update,score):
     answers = update.poll.options
 
     ret = False
@@ -125,8 +167,9 @@ def is_answer_correct(update):
             ret = True
             break
         counter = counter + 1
-
     return ret
+
+    
 
 
 # extract chat_id based on the incoming object
@@ -141,6 +184,24 @@ def get_chat_id(update, context):
     chat_id = context.bot_data[update.poll.id]
 
   return chat_id
+# Initialize the dictionary to store user scores
+user_scores = {}
+
+# Define a function to update the user score
+def update_score(user_id, score):
+    if user_id not in user_scores:
+        user_scores[user_id] = score
+    else:
+        user_scores[user_id] += score
+
+
+
+# Define a function to handle poll answers
+# def handle_poll_answer(bot, update):
+#     user_id = update.poll_answer.user.id
+#     option_ids = update.poll_answer.option_ids
+#     score = len(option_ids) # Each selected option is worth 1 point
+#     update_score(user_id, score)
 
 
 def error(update, context):
@@ -153,9 +214,10 @@ def main():
     updater = Updater(DefaultConfig.TELEGRAM_TOKEN,
                   use_context=True)
     dispatcher = updater.dispatcher
-    print('Welcome,Please use /start command to start me')
+   
     dispatcher.add_handler(CommandHandler("help", help_command_handler))
     dispatcher.add_handler(CommandHandler("start", start_command_handler))
+    dispatcher.add_handler(CommandHandler("quiz",quiz_command_handler))
 
     # message handler
     dispatcher.add_handler(MessageHandler(Filters.text, main_handler))
@@ -169,12 +231,21 @@ def main():
 
     # quiz answer handler
     dispatcher.add_handler(PollHandler(poll_handler, pass_chat_data=True, pass_user_data=True))
+    dispatcher.add_handler(PollAnswerHandler(poll_answer_handler))
 
     # log all errors
     dispatcher.add_error_handler(error)
 
    
     
+    # updater.start_webhook(
+    #     listen="0.0.0.0",
+    #     port=int(DefaultConfig.PORT),
+    #     url_path=DefaultConfig.TELEGRAM_TOKEN,
+    #     webhook_url=DefaultConfig.WEBHOOK_URL+DefaultConfig.TELEGRAM_TOKEN
+    # )
+
+    # logging.info(f"Start webhook mode on port {DefaultConfig.PORT}")
     updater.start_polling()
     logging.info(f"Start polling mode")
 
@@ -183,10 +254,10 @@ def main():
 
 
 class DefaultConfig:
-    PORT = int(os.environ.get("PORT", 3978))
-    TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN", "6049110046:AAGDMulUlNMrL-DCf2Qy2LLorYDCA6DQ4zQ")
+    PORT = int(os.environ.get("PORT", 8443))
+    TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN", "6049110046:AAHX0wlv39PRwEJRSIXAfZVIWDDoJwxwlZw")
     MODE = os.environ.get("MODE", "polling")
-    WEBHOOK_URL = os.environ.get("WEBHOOK_URL", "")
+    WEBHOOK_URL = os.environ.get("WEBHOOK_URL", "https://webhook.site/c09aeb51-af69-446b-93c7-a6d96c5d52d4")
 
     LOG_LEVEL = os.environ.get("LOG_LEVEL", "INFO").upper()
 
@@ -198,7 +269,7 @@ class DefaultConfig:
         )
 
 
-if __name__ == "__main__":
+if __name__== "__main__":
 
     # Enable logging
     DefaultConfig.init_logging()
